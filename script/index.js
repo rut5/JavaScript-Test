9949fcac32fab18f3aaf22eaafbcbbe589f63388
Rut <rutwintzell@outlook.com>
2025-10-13 13:57:49 +0200

    Added javascript

---
+++ script/index.js
@@ -0,0 +1,45 @@
+// exercise 1 
+
+console.log("Hello World")
+
+// exercise 2
+
+let myName; 
+myName = "Rut"
+
+let myAge;
+myAge = 20;
+
+let favColor;
+favColor = "green";
+
+console.log(`My name is ${myName} and I am ${myAge} years old. My favorite color is ${favColor}.`)
+
+// exercise 3
+
+let A;
+A = 8;
+
+let B;
+B = 7;
+
+console.log(A + B)
+
+console.log(A - B)
+
+console.log(A * B)
+
+console.log(A / B)
+
+// more
+
+if (A > B) {
+    console.log(true);
+} else {
+    console.log(false)
+}
+
+let students = 20;
+let extraStudents = students + 3;
+extraStudents = extraStudents % 2;
+console.log(students, extraStudents)
